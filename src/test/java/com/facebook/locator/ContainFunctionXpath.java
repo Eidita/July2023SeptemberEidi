0d477e065b60115584ea42345dd9bebf84e56ce8
Eidita <kamrunnaher0119@gmail.com>
2023-09-17 21:40:30 -0400

    ContainFunctionXpath

---
+++ src/test/java/com/facebook/locator/ContainFunctionXpath.java
@@ -0,0 +1,11 @@
+package com.facebook.locator;
+
+import org.testng.annotations.Test;
+
+public class ContainFunctionXpath {
+@Test
+public void containFunction() {
+	System.out.println("Git Branching");
+}
+
+}
